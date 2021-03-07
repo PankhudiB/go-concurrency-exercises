47a427c1493d44f5d2e9f48b6715243d43d0a8f7
Pankhudi <bpankhudi7@gmail.com>
2021-03-07 15:08:05 +0530

    Implement goroutines in different way. Put time.sleep for the main func to wait for goroutines to complete

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,20 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	go fun("goroutine func call")
 
 	// goroutine with anonymous function
+	go func() {
+		fun("goroutine-anonymous-func")
+	}()
 
 	// goroutine with function value call
+	funcValue := fun
+	go funcValue("goroutine-with-function-value-call")
 
 	// wait for goroutines to end
+	fmt.Println("waiting for go routine...")
+	time.Sleep(100 * time.Millisecond) // need this bcoz main routine is not waiting for the the go routine to finish as of now
 
 	fmt.Println("done..")
 }
